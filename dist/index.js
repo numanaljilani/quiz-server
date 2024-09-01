8c8d828a6a97c4f9cc4e92e854312961f88b6075
numanaljilani <numanaljilani001@gmail.com>
2024-09-01 13:32:07 +0530

    new commit

--- dist/index.js
+++ dist/index.js
@@ -9,10 +9,33 @@ const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
 const questionsRoutes_1 = __importDefault(require("./routes/questionsRoutes"));
 const errorHandler_1 = __importDefault(require("./middlewares/errorHandler"));
 const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 //The express.urlencoded() middleware is used to parse and extract this URL-encoded data from the request body and make it available in the req.body object for further processing in your application
 app.use(express_1.default.urlencoded({ extended: true }));
+// Define the path to the static HTML file
+const publicPath = path_1.default.join(__dirname, '..', 'public');
+// Serve static files from the 'public' directory
+app.use(express_1.default.static(publicPath));
+// Set up the default route to serve the HTML file
+// Set up the default route to serve the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path_1.default.join(publicPath, 'index.html'), (err) => {
+        if (err) {
+            console.error('Error sending file:', err);
+            res.status(500).send({ message: 'Internal server error' });
+        }
+    });
+});
+app.get('/privacy-policy', (req, res) => {
+    res.sendFile(path_1.default.join(publicPath, 'privacy.html'), (err) => {
+        if (err) {
+            console.error('Error sending file:', err);
+            res.status(500).send({ message: 'Internal server error' });
+        }
+    });
+});
 app.get("/", (req, res) => {
     res.send("<h2>Salon server is working</h2>");
 });
--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import authRoutes from "./routes/authRoutes";
 import questionsRoutes from "./routes/questionsRoutes";
 import errorHandler from "./middlewares/errorHandler";
 import cors from 'cors'
+import path from 'path'
 
 
 app.use(express.json());
@@ -12,6 +13,30 @@ app.use(cors())
 
 //The express.urlencoded() middleware is used to parse and extract this URL-encoded data from the request body and make it available in the req.body object for further processing in your application
 app.use(express.urlencoded({ extended: true }));
+
+// Define the path to the static HTML file
+const publicPath = path.join(__dirname, '..', 'public');
+// Serve static files from the 'public' directory
+app.use(express.static(publicPath));
+// Set up the default route to serve the HTML file
+// Set up the default route to serve the HTML file
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path.join(publicPath, 'index.html'), (err) => {
+    if (err) {
+      console.error('Error sending file:', err);
+      res.status(500).send({ message: 'Internal server error' });
+    }
+  });
+});
+app.get('/privacy-policy', (req: Request, res: Response) => {
+  res.sendFile(path.join(publicPath, 'privacy.html'), (err) => {
+    if (err) {
+      console.error('Error sending file:', err);
+      res.status(500).send({ message: 'Internal server error' });
+    }
+  });
+});
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("<h2>Salon server is working</h2>");
